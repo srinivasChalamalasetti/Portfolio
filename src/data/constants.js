28ac3a808947a1bfd24254b830627e08617dd95d
Chalamalasetti Naga Srinivas <106512435+srinivasChalamalasetti@users.noreply.github.com>
2024-04-23 20:12:13 +0530

    Add files via upload

---
+++ src/data/constants.js
@@ -0,0 +1,205 @@
+export const Bio = {
+  name: "Naga Srinivas",
+  roles: [
+    "Frontend Developer",
+    "Programmer"
+  ],
+  description:
+    "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+  github: "https://saifurrahman95.com",
+  resume:
+  "https://drive.google.com/drive/folders/14gNxfaD3_3OegpsFoEkRhucKCtNRSuhb",
+  linkedin: "https://www.linkedin.com",
+  twitter: "https://twitter.com",
+  insta: "https://www.instagram.com/",
+  facebook: "https://www.facebook.com",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      
+     
+      
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "Bootstrap",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+      
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "Node Js",
+        image: "https://nodejs.org/static/images/logo.svg",
+      },      
+    ],
+  },
+  {
+    title: "Programming Languages",
+    skills: [ 
+      {
+        name: "C", 
+        image: 
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/c/c-original.svg",
+      },
+      {
+        name: "Java",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg",
+      }, 
+      {
+        name: "Python",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+      },
+    ],
+  },
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+        image:
+          "https://e7.pngegg.com/pngimages/713/558/png-clipart-computer-icons-pro-git-github-logo-text-logo-thumbnail.png",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "https://thumbs.dreamstime.com/b/web-development-coding-programming-internet-technology-business-concept-web-development-coding-programming-internet-technology-121903546.jpg",
+    role: "Web Development Industrial Training ",
+    company: "",
+    date: "June 2023 - Nov 2023",
+    desc: "I completed a six-month industrial training program in web development facilitated by APSSDC. Throughout the program, I immersed myself in learning various aspects of web development, including coding, design principles, and project management. Under the guidance of APSSDC instructors.",
+    skills: [
+      "HTML",
+      "CSS",
+      "Javascript",
+      "PHP",
+    ],
+   
+  },
+];
+
+export const education = [
+  {
+    id: 0,
+    school: " Anil Neerukonda Institute of Techonology and Sciences",
+    date: "Jun 2021 - April 2024",
+    grade: "8.07 CGPA",
+    desc: "I completed my B.Tech in Computer Science with a specialization in AI and ML at Anits College in Vishakhapatnam.",
+  },
+  {
+    id: 1,
+    school: "A. A. N. M. & V. V. R. S. R. Polytechnic",
+    date: "Jun 2018 - Nov 2021",
+    grade: "85%",
+    desc: "I completed My diploma in Computer Science at A. A. N. M. & V. V. R. S. R. Polytechnic.",
+  },
+  {
+    id: 2,
+    school: "Zillal Parishat High School",
+    date: "Jun 2017 - April 2018",
+    grade: "92.3%",
+    desc: "I completed my 10th class in SSC at Zillal Parishat High School",
+  },
+];
+
+export const projects = [
+  {
+    id: 11,
+    title: "Car Rental",
+    date: "Oct 2022 - Nov 2022",
+    description:
+      "I had the opportunity to work on an exciting and challenging Car Rental Web Development project, which allowed me to showcase my skills in creating a user-friendly and efficient online platform for car rentals. We are four members of the team and In this project I designed the front end part which includes login page and several other pages of cars.",
+    image:
+      "https://www.sourcecodester.com/sites/default/files/2020-04/images/online-car-rental.jpg",
+    tags: [
+      "HTML",
+      "CSS",
+      "PHP",
+      "Javascript",
+    ],
+    category: "web app",
+   
+  },
+  {
+    id: 9,
+    title: "Online Music Platform",
+    date: "Aug 2023 - Sep 2023",
+    description:
+      "As a Front-End and Back-End Developer, I played a pivotal role in the creation and implementation of this platform. My responsibilities included designing and developing the user interface, ensuring smooth navigation, and optimizing the overall user experience. Additionally, I actively worked on the back-end to handle data management, user authentication, and content delivery",
+    image:
+      "https://peachmode.com/cdn/shop/articles/elements-of-indian-music-peachmode.jpg?v=1668998980",
+    tags: [
+      "HTML",
+      "CSS",
+      "Javascript",
+      "PHP",
+    ],
+    category: "web app",
+   
+  },
+  {
+    id: 0,
+    title: "Travelling Page",
+    date: "Nov 2023 - Dec 2023",
+    description:
+      "Implemented responsive design principles to ensure seamless user experiences across various devices As a passionate and innovative recent graduate, I undertook the development of a captivating travel web page using React. Leveraging my foundational knowledge in web development and React, I designed and implemented an intuitive user interface that seamlessly guides users through their travel journey.",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRpaQMxKOEX15Gs_6M-ZGZsHrwgim79pqzTHw&s",
+    tags: ["React Js", "Node Js"],
+    category: "web page",
+    
+  },
+  {
+    id: 1,
+    title: "My Portfolio",
+    date: "Feb 2024 - Feb 2024",
+    description:"I built My portfolio, using React, epitomizes my expertise in web development. With clean design and seamless navigation, it showcases my skills and projects effectively. Utilizing React's capabilities, I've ensured a responsive and engaging experience for visitors, reflecting my dedication to frontend excellence.",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS2ZWQlo8v2szeR55CNOTMHZHTirPtsHEU2Qg&s",
+    tags: [
+      "React Js",
+      "Node Js",
+    ],
+    category: "web app",
+  },
+];
